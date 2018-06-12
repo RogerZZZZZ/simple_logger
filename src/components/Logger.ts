c1ba05df7fd47219383922aa33a449f8c48d55bb
RogerZZZZZ <501841352@qq.com>
2018-06-12 22:54:33 +0800

    remove: previous init function

--- src/components/Logger.ts
+++ src/components/Logger.ts
@@ -10,12 +10,9 @@ export namespace Logger {
     Object.assign(configObject, opt)
   }
 
-  // PVModule.init(config)
   export const PV = new PVModule(configObject)
 
-  // UVModule.init(config)
   export const UV = new UVModule(configObject)
 
-  // EventModule.init(config)
   export const EventLogger = new EventModule(configObject)
 }
