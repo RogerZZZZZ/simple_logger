83ff2659bbeb861ac1a62cb569825f2e582ef06d
RogerZZZZZ <501841352@qq.com>
2018-06-15 08:44:17 +0800

    add some basic implementation of uv log

--- src/components/modules/model.ts
+++ src/components/modules/model.ts
@@ -14,3 +14,11 @@ export interface RequiredModel {
     tag: string,
     userName?: string
 }
+
+export interface ModelUv {
+    tag: string,
+    platForm?: string,
+    userName?: string,
+    os?: string,
+    version?: string,
+}
